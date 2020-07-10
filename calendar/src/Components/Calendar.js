80ac37545daeab768d2bfab7f3649153d67077f1
vanceleon <vanceleon44@gmail.com>
2020-07-10 18:03:39 -0400

    header function in calender component

--- calendar/src/Components/Calendar.js
+++ calendar/src/Components/Calendar.js
@@ -1,8 +1,32 @@
 import React, { useState } from 'react';
 import dateFns from 'date-fns';
-import './Calendar.css';
+import '../css/Calendar.css';
 
 export default function Calendar() {
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const header = () => {
+    const dateFormat = 'MMMM YYYY';
+    return (
+      <div className='header row flex-middle'>
+        <div className='column col-start'>
+          <div className='icon' onClick={prevMonth}>
+            chevron_left
+          </div>
+        </div>
+        <div className='column col-center'>
+          <span>{dateFns.format(currentDate, dateFormat)}</span>
+        </div>
+        <div className='column col-end'>
+          <div className='icon' onClick={nextMonth}>
+            chevron_right
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className='calendar'>
       <div>{header()}</div>
